c5a03e239b231a1925a64872e3faddfea9b2ae26
daofor <im.jadeydi@gmail.com>
2022-06-26 22:34:51 +0800

    more links

---
+++ airdrops.ts
@@ -0,0 +1,20 @@
+const Data = [
+  {
+    name: 'airdrops',
+    link: 'https://airdrops.io/',
+  },
+  {
+    name: 'icodrops',
+    link: 'https://icodrops.com/',
+  },
+  {
+    name: 'dropsearn',
+    link: 'https://dropsearn.com/',
+  },
+  {
+    name: 'airdropbob',
+    link: 'https://www.airdropbob.com/',
+  },
+];
+
+export default Data;
