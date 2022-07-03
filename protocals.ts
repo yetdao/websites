cdfd598c35382c418e0319d70a94047bbccc0515
daofor <im.jadeydi@gmail.com>
2022-07-03 16:21:45 +0800

    more links

---
+++ protocals.ts
@@ -0,0 +1,16 @@
+const Data = [
+  {
+    name: 'Curve',
+    link: 'https://curve.fi/',
+  },
+  {
+    name: 'totle',
+    link: 'https://www.totle.com/',
+  },
+  {
+    name: 'yearn',
+    link: 'https://yearn.finance/#/portfolio',
+  },
+];
+
+export default Data;
